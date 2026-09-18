078b7b21454278ad9eff2bfdae5be9163c26aa4a
Sameer Sahu <sameer.sahu2025@vitstudent.ac.in>
2026-09-19 05:17:02 +0530

    Optimize memory and downscale input image

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import torch
 import urllib.request
 import gradio as gr
 from fastai.vision.all import *
@@ -17,20 +18,21 @@ learn = load_learner(MODEL_FILE)
 labels = learn.dls.vocab
 
 def predict(img):
-    img = PILImage.create(img)
-    pred, pred_idx, probs = learn.predict(img)
-    return {labels[i]: float(probs[i]) for i in range(len(labels))}
+    if isinstance(img, Image.Image):
+        img = img.resize((224, 224))
+    else:
+        img = Image.fromarray(img).resize((224, 224))
 
-image = gr.Image(type="pil")
-label = gr.Label(num_top_classes=3)
+    pred, pred_idx, probs = learn.predict(img)
+    return {learn.dls.vocab[i]: float(probs[i]) for i in range(len(probs))}
+
+image_input = gr.Image(type="pil")
 
 demo = gr.Interface(
     fn=predict,
-    inputs=image,
-    outputs=label,
-    title="Houseplant Identifier",
-    description="Take a photo of a leaf or upload an image to identify the houseplant species."
+    inputs=image_input,
+    outputs=gr.Label(num_top_classes=3),
+    title="Houseplant Classifier"
 )
 
-if __name__ == "__main__":
-    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+demo.launch(server_name="0.0.0.0", server_port=10000)
