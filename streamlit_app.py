b5727df835b701e06288b0b0a15b6ea755e43a10
Sameer Sahu <sameer.sahu2025@vitstudent.ac.in>
2026-09-20 01:44:22 +0530

    Update species list

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def load_model():
 learn, vocab = load_model()
 
 # --- Supported Species List ---
-with st.expander(f"🌿📋 The {len(learn.dls.vocab)} plant species that I can predict!"):
+with st.expander(f"🌿📋 The {len(learn.dls.vocab)} plant species that I can identify!"):
     sorted_species = sorted(vocab)
     # Split the names evenly across 2 columns
     col1, col2 = st.columns(2)
